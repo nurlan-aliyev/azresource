ca3a650e0774963dc84273cba9f4909cf95cbaa1
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2021-07-25 11:38:58 +0400

    Create 2_ededler_cemi.py

---
+++ Python/Kod nümunələri/2_ededler_cemi.py
@@ -0,0 +1,34 @@
+"""
+Python dili üzrə kod nümunələri - müəllif Nurlan Aliyev,  25/07/2021
+
+Kod №2 - İki ədədin cəmi
+
+Kopyalayıb kodu test edə bilərsiniz
+
+Və yaxud bu link vasitəsilə yoxlaya bilərsiniz  https://replit.com/@NurlanAliyev/2ededlercemipy?v=1
+"""
+
+# Birinci variant
+
+eded_1 = 5
+eded_2 = 8
+
+#Toplama əməliyyatı
+
+cem = eded_1 + eded_2
+
+# Nəticənin çap olunması
+
+print(f'{eded_1} + {eded_2} = {cem}')
+
+# İkinci variant, ədədlərin istifadəçi tərəfindən verilməsi (yuxarı sətrdəki kodu şərhə çevirib aşağıdakı sətrlərin şərh simvollarını silin!)
+
+# Ədədlərin oxunması və yaddaşda saxlanılması
+# eded_1 = int(input("İlk ededi qeyd edin: "))
+# eded_2 = int(input("İkinci ededi qeyd edin: "))
+
+# Ədədlərin toplanması
+# cem = eded_1 + eded_2
+
+# Nəticənin çap olunması
+# print(f'{eded_1} + {eded_2} = {cem}')
