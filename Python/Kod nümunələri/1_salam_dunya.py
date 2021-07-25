123ad4690b6603c5cfd4a2e334cc4dc2695c28d0
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2021-07-25 11:41:50 +0400

    Update and rename 1salamdunya.py to 1_salam_dunya.py

--- Python/Kod nümunələri/1_salam_dunya.py
+++ Python/Kod nümunələri/1_salam_dunya.py
@@ -1,9 +1,11 @@
 """
-Python dili üzrə kod nümunələri - müəllif Nurlan Aliyev
+Python dili üzrə kod nümunələri - müəllif Nurlan Aliyev,  25/07/2021
 
 Kod №1 - Salam, Dünya!
 
 Kopyalayıb kodu test edə bilərsiniz
+
+Və yaxud bu link vasitəsilə yoxlaya bilərsiniz  https://replit.com/@NurlanAliyev/1salamdunya
 """
 
 print("Salam, Dünya!")
