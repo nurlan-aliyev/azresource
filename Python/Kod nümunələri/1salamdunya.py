1ee1fb86c6c2541a892398c34f5dd67a0de9676f
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2021-07-25 11:19:41 +0400

    Create 1salamdunya.py

---
+++ Python/Kod nümunələri/1salamdunya.py
@@ -0,0 +1,11 @@
+"""
+Python dili üzrə kod nümunələri - müəllif Nurlan Aliyev
+
+Kod №1 - Salam, Dünya!
+
+Kopyalayıb kodu test edə bilərsiniz
+"""
+
+print("Salam, Dünya!")
+
+#Yaxşı olar ki, PyCharm, Atom, SublimeeText və VScode tipli editorlardan istifadə edəsiniz
