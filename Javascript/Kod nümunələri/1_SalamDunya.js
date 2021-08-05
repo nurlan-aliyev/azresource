10fcac2a70b1ca1c12c43c4bbe2a61263c17fb41
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2021-08-05 22:42:33 +0400

    Create 1_SalamDunya.js

---
+++ Javascript/Kod nümunələri/1_SalamDunya.js
@@ -0,0 +1,10 @@
+/*
+JavaScript dili üzrə kod nümunələri - müəllif Nurlan Aliyev,  05/08/2021
+Kod №1 - Salam, Dünya!
+Kopyalayıb kodu test edə bilərsiniz
+Və yaxud bu link vasitəsilə yoxlaya bilərsiniz  https://replit.com/@NurlanAliyev/1SalamDunya#index.js
+*/
+
+console.log("Salam, Dünya!")
+
+// Node.js endirilməyibsə brauzerdən yoxlaya bilərsiniz (Ctrl + Shift + j)
