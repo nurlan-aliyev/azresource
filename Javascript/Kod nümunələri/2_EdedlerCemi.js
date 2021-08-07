1771d4ecc6c4e6051fe97eaa95bc0038d490d469
Nurlan Aliyev <nurlan.aliyev58@gmail.com>
2021-08-07 14:22:46 +0400

    Create 2_EdedlerCemi.js

---
+++ Javascript/Kod nümunələri/2_EdedlerCemi.js
@@ -0,0 +1,28 @@
+/*
+JavaScript dili üzrə kod nümunələri - müəllif Nurlan Aliyev,  07/08/2021
+Kod №2 - Ədədlər cəmi
+Kopyalayıb kodu test edə bilərsiniz
+Və yaxud bu link vasitəsilə yoxlaya bilərsiniz  https://replit.com/@NurlanAliyev/2EdedlerCemi#index.js
+*/
+
+//birinci variant
+//Ədədlərin təyini
+const eded1 = 5
+const eded2 = 8 
+//ədədlər cəminin hesablanması
+const cem = eded1 + eded2
+
+//Nəticənin çap edilməsi
+console.log(`${eded1} ilə ${eded2} cəmi ${cem}-a(ə) bərabərdir`)
+
+
+//İkinci variant, yuxarıdakı kod sətrlərini şərh blokuna çevirin və yaxud silin
+//daxil edilən ədədlərin yaddaşda saxlanılması
+const eded_1 = parseInt(prompt('İlk ədədi daxil edin: '));
+const eded_2 = parseInt(prompt('İkinci ədədi daxil edin: '));
+
+//ədədlərin cəmlənməsi
+const cem_2 = eded_1 + eded_2;
+
+// Nəticənin çap olunması
+console.log(`${eded_1} ilə ${eded_2} cəmi ${cem_2}-a(ə) bərabərdir`);
